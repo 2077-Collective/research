03615aef8557a00713898371e198e0f68c360f28
LilFatFrank <karan.parsnani@ymail.com>
2025-01-09 16:43:48 +0530

    chore(fix): applying danneh's feedback

--- src/lib/components/ui/ArticleSpotlight.svelte
+++ src/lib/components/ui/ArticleSpotlight.svelte
@@ -19,20 +19,20 @@
 	<div class="flex flex-col lg:flex-row">
 		<img src={article.thumb} alt={article.title} class="w-full h-auto lg:w-4/6 object-cover" />
 		<div
-			class="flex flex-col gap-3 md:gap-6 w-full lg:w-2/6 flex flex-col p-6 md:p-10 text-base tracking-normal bg-secondary max-md:px-5 max-md:max-w-full"
+			class="flex flex-col gap-3 md:gap-6 w-full lg:w-2/6 p-6 md:p-10 text-base bg-secondary max-md:px-5 max-md:max-w-full"
 		>
-			<div class="flex gap-1 items-start w-full text-sm leading-none">
+			<div class="flex gap-1 items-start w-full text-sm leading-none tracking-wide">
 				{#each article.categories as category}
 					<Badge variant="outline">{category.name}</Badge>
 				{/each}
 			</div>
-			<h1 class="font-soehne text-5xl font-medium max-md:text-3xl leading-[57px]">
+			<h1 class="font-soehne text-5xl font-medium max-md:text-3xl leading-[57px] tracking-tight">
 				{article.title}
 			</h1>
-			<p class="leading-7 dark:text-white">
+			<p class="leading-7 dark:text-white tracking-normal">
 				{article.summary}
 			</p>
-			<p class="font-medium dark:text-white">
+			<p class="font-medium dark:text-white tracking-normal">
 				By {getAuthorsText(article.authors)}
 			</p>
 		</div>
