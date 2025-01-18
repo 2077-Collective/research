9e8bd01b35c8fa434166a2abfeb8c7c44f42915e
Lyn <0xlynett@proton.me>
2025-01-18 17:36:54 +0000

    feat: change card bios

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 
 <BaseHead />
 
-<div class="px-3 md:px-12 flex flex-col">
+<div class="px-3 md:px-12 flex flex-col gap-8">
 	<section>
 		<h2
 			id="about-us"
@@ -13,7 +13,7 @@
 		>
 			About us
 		</h2>
-		<div class="md:text-lg leading-relaxed mb-6 max-w-screen-lg space-y-4 text-pretty">
+		<div class="md:text-lg leading-relaxed max-w-screen-lg space-y-4 text-pretty">
 			<p>
 				2077 Research is a research firm dedicated to producing in-depth analysis and insights on
 				decentralized tech and the web3 movement. We collaborate with industry leaders to deliver
@@ -31,20 +31,20 @@
 			id="team"
 			class="text-2xl md:text-4xl font-medium leading-9 mb-4 md:mb-8 font-soehne tracking-tight"
 		>
-			Our team
+			Core
 		</h2>
 		<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
 			<TeamCard
 				twitter_username="eawosikaa"
 				full_name="Emmanuel Awosika"
 				role="CEO"
-				summary="Emmanuel is a very cool person. He reads a lot of LessWrong and yaps far too much in my DMs."
+				summary="Chaos agent. Learning machine. Crazy genius. Ethernaut making Ethereum cool again."
 			/>
 			<TeamCard
 				twitter_username="lynettdoteth"
 				full_name="Lynett Simons"
-				role="Shitposter"
-				summary="Lyn is a deeply unprofessional software engineer working at 2077 Labs. In her free time, she chases pigeons."
+				role="Developer"
+				summary="i chase pigeons for fun"
 			/>
 		</div>
 	</section>
