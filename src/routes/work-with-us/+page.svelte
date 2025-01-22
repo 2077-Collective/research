c98748b7a57e91413efa19b39b430fda03c24a57
Lyn <0xlynett@proton.me>
2025-01-22 19:39:05 +0000

    feat: work with us page

--- src/routes/work-with-us/+page.svelte
+++ src/routes/work-with-us/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import BaseHead from '$lib/components/server/BaseHead.svelte';
 	import Accordion from '$lib/components/ui/accordion/accordion.svelte';
-	import ContributorCard from '$lib/components/ui/ContributorCard.svelte';
-	import TeamCard from '$lib/components/ui/TeamCard.svelte';
+	import Testimonials from '$lib/components/ui/Testimonials.svelte';
 </script>
 
 <BaseHead />
@@ -10,7 +9,7 @@
 <div class="px-3 md:px-12 flex flex-col gap-8">
 	<section>
 		<h1
-			id="about-us"
+			id="work-with-us"
 			class="text-3xl md:text-5xl font-medium leading-9 mb-4 md:mb-8 font-soehne tracking-tight"
 		>
 			Work with us
@@ -18,14 +17,21 @@
 		<div class="md:text-lg leading-relaxed max-w-screen-lg space-y-4 text-pretty">
 			<p>
 				2077 Research can work with your DeFi protocol or other crypto project to produce neutral,
-				unbiased technical reports that thousands read.
+				unbiased technical reports. If you are a protocol interested in working with us, you can
+				contact us via email: <a
+					href="mailto://research@2077.xyz"
+					class="text-blue-500 hover:underline">research@2077.xyz</a
+				>.
+			</p>
+			<p>
+				Our research has reached over 150,000 people and been shared by the community 300+ times.
 			</p>
 		</div>
 	</section>
 	<section>
 		<h2
 			id="faq"
-			class="text-2xl md:text-4xl font-medium leading-9 mb-4 md:mb-8 font-soehne tracking-tight"
+			class="text-2xl md:text-4xl font-medium leading-9 mb-2 md:md-4 font-soehne tracking-tight"
 		>
 			FAQ
 		</h2>
@@ -33,19 +39,23 @@
 		<Accordion
 			items={[
 				{
-					title: 'Who is your research for?',
-					content: 'We target technical people.'
+					title: 'Who is our research for?',
+					content: 'Lorem ipsum odor amet, consectetuer adipiscing elit.'
 				},
 				{
 					title: 'Meow?',
-					content: ''
+					content:
+						'Lorem ipsum odor amet, consectetuer adipiscing elit. Eros vestibulum accumsan in condimentum purus sollicitudin laoreet. Et facilisis egestas maecenas gravida hac porttitor commodo conubia. Metus donec augue, sed diam varius hendrerit. Vitae aliquet efficitur volutpat eleifend tincidunt diam nulla aptent donec.'
 				},
 				{
 					title: 'Accordion Item 3',
 					content:
-						"That's a pretty fucking good milkshake. I dont know if it's worth five dollars, but it's pretty fucking good."
+						'At tortor nullam ultrices nisi interdum facilisi semper. Et himenaeos viverra eleifend dolor euismod commodo. Cras gravida lobortis scelerisque maximus dui nulla.'
 				}
 			]}
 		/>
+	</section>
+	<section>
+		<Testimonials />
 	</section>
 </div>
