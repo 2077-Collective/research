26fbd4b64f1714ac08e132eb76f636c74e067358
Lyn <0xlynett@proton.me>
2025-01-28 18:01:48 +0000

    feat: click on badge to enter category

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -583,12 +583,14 @@
 					<section class="flex flex-col w-full">
 						<div class="flex flex-wrap gap-2 font-mono">
 							{#each article.categories as category}
-								<Badge
-									variant="rectangular"
-									class="bg-black/50 text-white border-white/20 text-xs lg:text-sm"
+								<a href={'/category/' + category.name.toLowerCase()}
+									><Badge
+										variant="rectangular"
+										class="bg-black/50 text-white border-white/20 text-xs lg:text-sm"
+									>
+										{category.name}
+									</Badge></a
 								>
-									{category.name}
-								</Badge>
 							{/each}
 						</div>
 
