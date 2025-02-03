a3399a89d7a4440f72af45b117811f99036365c2
ayoolawp <danielayoola@techcitadels.com>
2025-02-03 20:44:28 +0100

    strapi

--- config/database.js
+++ config/database.js
@@ -32,7 +32,7 @@ module.exports = ({ env }) => {
         password: env('DATABASE_PASSWORD', 'password'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined), 
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
