efcfb379a61c4f04e2a317c35a4ca5ba2946b6d7
Moses Enyinnaya <moscolyno98@gmail.com>
2025-02-07 08:50:35 +0100

    added scroll to top button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-	import '../app.css';
-	import Nav from '$lib/components/ui/Nav.svelte';
 	import Beehiiv from '$lib/components/ui/Beehiiv.svelte';
 	import Footer from '$lib/components/ui/Footer.svelte';
-	import Analytics from '$lib/components/server/Analytics.svelte';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import Nav from '$lib/components/ui/Nav.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { ArrowUp } from 'lucide-svelte';
+	import '../app.css';
 
 	injectAnalytics();
+
+	let showButton = $state(false);
+
+	function scrollContainer() {
+		return document.documentElement || document.body;
+	}
+
+	function handleScroll() {
+		showButton = scrollContainer().scrollTop > 600;
+	}
+
+	function handleScrollToTop() {
+		document.body.scrollIntoView({ behavior: 'smooth' });
+	}
 
 	const { children } = $props();
 </script>
@@ -15,6 +29,8 @@
 <svelte:head>
 	<link rel="icon" type="image/svg" href="/favicon.svg" />
 </svelte:head>
+
+<svelte:window on:scroll={handleScroll} />
 
 <div>
 	<Nav />
@@ -27,6 +43,21 @@
 		<Beehiiv variant="home" />
 		<Footer />
 		<Toaster />
+
+		{#if showButton}
+			<button
+				class="flex items-center gap-1 px-4 py-2 text-2xl transition-colors duration-300 group text-[12.667px] h-10 bg-[#19191A] rounded-[43px] text-[#B4B4B4] group fixed bottom-12 md:bottom-11 right-3 md:right-11 font-semibold shadow-2xl border border-[#333] z-[999999]"
+				aria-label="View all research articles"
+				onclick={handleScrollToTop}
+			>
+				<span class="max-md:hidden">Back to the Top</span>
+
+				<ArrowUp
+					class="size-5 rounded-full group-hover:-translate-y-[2px] transition"
+					style="stroke-width: 1.4"
+				/>
+			</button>
+		{/if}
 	</div>
 </div>
 
