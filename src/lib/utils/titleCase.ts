9a4572c155f6e2a43f936af37faca6cbe7e92c47
ndu <chijindujulian@gmail.com>
2025-02-08 08:42:38 +0100

    fix title

--- src/lib/utils/titleCase.ts
+++ src/lib/utils/titleCase.ts
@@ -1,6 +1,6 @@
 export function toTitleCase(str: string): string {
     const minorWords = new Set(['a', 'an', 'the', 'and', 'but', 'or', 'for', 'nor', 'on', 'at', 'to', 'with', 'in']);
-    const technicalTerms = new Set(['eip', 'eth', 'dao', 'defi', 'nft']);
+    const technicalTerms = new Set(['eip']);
     const romanNumeralPattern = /(^|\s|-)(I{1,3}|IV|V|VI{1,3}|IX|X)(\s|$|:)/i;
     const hyphenatedEipPattern = /^(eip)-(\d+)$/i;
 
