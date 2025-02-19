db7b80b393f64e84e392f9d101bc1c79aa8fc1c1
Moses Enyinnaya <moscolyno98@gmail.com>
2025-02-19 16:18:56 +0100

    minor fix

--- src/lib/components/ui/JoinSection.svelte
+++ src/lib/components/ui/JoinSection.svelte
@@ -8,88 +8,14 @@
 
 	let api: CarouselAPI | undefined;
 	let current = 0;
-	let count = 0;
 
 	$: if (api) {
-		count = api.scrollSnapList()?.length ?? 0;
 		current = (api.selectedScrollSnap?.() ?? 0) + 1;
 
 		api.on?.('select', () => {
 			current = (api?.selectedScrollSnap?.() ?? 0) + 1;
 		});
 	}
-
-	const TWEEN_FACTOR_BASE = 0.52;
-	let tweenFactor = 0;
-	let tweenNodes: HTMLElement[] = [];
-
-	const numberWithinRange = (number: number, min: number, max: number): number =>
-		Math.min(Math.max(number, min), max);
-
-	const setTweenNodes = (emblaApi: CarouselAPI): void => {
-		tweenNodes = emblaApi.slideNodes().map((slideNode) => {
-			return slideNode.querySelector('.embla__slide__number') as HTMLElement;
-		});
-	};
-
-	const setTweenFactor = (emblaApi: CarouselAPI): void => {
-		tweenFactor = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
-	};
-
-	const tweenScale = (emblaApi: CarouselAPI, eventName?: any): void => {
-		const engine = emblaApi.internalEngine();
-		const scrollProgress = emblaApi.scrollProgress();
-		const slidesInView = emblaApi.slidesInView();
-		const isScrollEvent = eventName === 'scroll';
-
-		emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-			let diffToTarget = scrollSnap - scrollProgress;
-			const slidesInSnap = engine.slideRegistry[snapIndex];
-
-			slidesInSnap.forEach((slideIndex) => {
-				if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
-
-				if (engine.options.loop) {
-					engine.slideLooper.loopPoints.forEach((loopItem) => {
-						const target = loopItem.target();
-
-						if (slideIndex === loopItem.index && target !== 0) {
-							const sign = Math.sign(target);
-
-							if (sign === -1) {
-								diffToTarget = scrollSnap - (1 + scrollProgress);
-							}
-							if (sign === 1) {
-								diffToTarget = scrollSnap + (1 - scrollProgress);
-							}
-						}
-					});
-				}
-
-				const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor);
-				const scale = numberWithinRange(tweenValue, 0, 1).toString();
-				const tweenNode = tweenNodes[slideIndex];
-				tweenNode.style.transform = `scale(${scale})`;
-			});
-		});
-	};
-
-	export const setupTweenScale = (emblaApi: CarouselAPI): (() => void) => {
-		setTweenNodes(emblaApi);
-		setTweenFactor(emblaApi);
-		tweenScale(emblaApi);
-
-		emblaApi
-			.on('reInit', setTweenNodes)
-			.on('reInit', setTweenFactor)
-			.on('reInit', tweenScale)
-			.on('scroll', tweenScale)
-			.on('slideFocus', tweenScale);
-
-		return (): void => {
-			tweenNodes.forEach((slide) => slide.removeAttribute('style'));
-		};
-	};
 
 	const socials = [
 		{
@@ -142,7 +68,7 @@
 				{@const isRight = current === 6 ? 1 : current + 1}
 
 				<Carousel.Item
-					class={cn('relative flex-none w-[300px] md:w-[370px] mr-2.5 md:mr-5 p-0')}
+					class={cn('relative flex-none w-[300px] md:w-[397px] mr-2.5 md:mr-5 p-0')}
 					style="perspective: 1000px; transform-style: preserve-3d;"
 				>
 					<div
