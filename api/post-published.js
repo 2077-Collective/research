97f1bee6178a61167386a529de43d0f7426c9097
ndu <chijindujulian@gmail.com>
2025-03-14 13:30:34 +0100

    add fix

--- api/post-published.js
+++ api/post-published.js
@@ -1,4 +1,4 @@
-const algoliasearch = require('algoliasearch');
+const { default: algoliasearch } = require('algoliasearch');
 
 const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
 const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
