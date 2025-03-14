b2073c50aacf1245057782a31420664f3657ca9f
ndu <chijindujulian@gmail.com>
2025-03-14 12:57:09 +0100

    add fix

--- api/post-deleted.js
+++ api/post-deleted.js
@@ -1,4 +1,4 @@
-import { default as algoliasearch } from 'algoliasearch';
+import { algoliasearch } from 'algoliasearch';
 
 const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
 const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
